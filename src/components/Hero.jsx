8e88529ab6286e6916a78fe0e658b2f43286ec9a
Linwei Gao <jogao1983@gmail.com>
2023-03-21 22:56:29 -0700

    add Logo

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,11 +16,11 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#915EFF]'>Adrian</span>
+            Hi, I'm <span className='text-[#915EFF]'>Linwei</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop 3D visuals, user <br className='sm:block hidden' />
-            interfaces and web applications
+            I develop from FrontEnd apps <br className='sm:block hidden' />
+            to BackEnd service, database, big data and more...
           </p>
         </div>
       </div>
